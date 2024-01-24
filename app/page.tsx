ce0e668d4ab9feb75575a85505f57788d88b8c38
Vishal Kashi <dev.vishalkashi@gmail.com>
2024-01-24 21:47:50 +0530

    fix: redirect user to homefeed if already logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from 'next/image'
+import { auth } from "@/auth/auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+const Home = async () => {
+  const session = await auth();
+
+  if (session?.user?.id) {
+    redirect("/home");
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        
-    </main>
-  )
-}
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+  );
+};
+
+export default Home;
