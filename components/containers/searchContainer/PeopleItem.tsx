13e1d43ae8503653599742515c73bf5a8de3e8bb
Vishal Kashi <dev.vishalkashi@gmail.com>
2024-02-03 19:42:13 +0530

    feat: go to user profile after clicking on search user page

--- components/containers/searchContainer/PeopleItem.tsx
+++ components/containers/searchContainer/PeopleItem.tsx
@@ -1,11 +1,22 @@
+"use client";
 import React from "react";
-import { Avatar, Button } from "@nextui-org/react";
+import { Avatar, Button, Skeleton } from "@nextui-org/react";
 import { getUserInitials } from "@/utils/helper";
 import { UserType } from "@/database/schema/users";
+import { useRouter } from "next/navigation";
 
 const PeopleItem = ({ user }: { user: UserType }) => {
+  const router = useRouter();
+
+  const onUserClick = () => {
+    router.push(`/profile/${user.id}`);
+  };
+
   return (
-    <div className="flex gap-4 pt-4 w-full cursor-pointer">
+    <div
+      className="flex gap-4 pt-4 w-full hover:bg-zinc-900/30 hover:cursor-pointer px-4"
+      onClick={onUserClick}
+    >
       <div className="mt-1">
         <Avatar
           src={user?.image ?? ""}
@@ -18,8 +29,10 @@ const PeopleItem = ({ user }: { user: UserType }) => {
           <p className="text-secondary">{user.name}</p>
           <p className="text-sm mt-2">2,312 followers</p>
         </div>
-        <div className="mt-2">
-          <Button variant="outline">Follow</Button>
+        <div className="flex h-full flex-col justify-center">
+          <Button radius="sm" size="sm">
+            Follow
+          </Button>
         </div>
       </div>
     </div>
